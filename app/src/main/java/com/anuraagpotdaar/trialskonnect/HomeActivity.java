a5c5d5cce4b92ee01eb2d67535416b2ae4727764
Mansur <mansurkhan55555@gmail.com>
2022-03-11 19:30:32 +0530

    User Login

--- app/src/main/java/com/anuraagpotdaar/trialskonnect/HomeActivity.java
+++ app/src/main/java/com/anuraagpotdaar/trialskonnect/HomeActivity.java
@@ -1,14 +1,50 @@
 package com.anuraagpotdaar.trialskonnect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        replaceFragment(new DashboardFragment());
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.action_home:
+                        replaceFragment(new DashboardFragment());
+                        break;
+                    case R.id.action_watch:
+                        replaceFragment(new DeviceFragment());
+                        break;
+                    case R.id.action_calendar:
+                        replaceFragment(new MedsCalenderFragment());
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void replaceFragment (Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.home_frame,fragment);
+        fragmentTransaction.commit();
     }
 }
